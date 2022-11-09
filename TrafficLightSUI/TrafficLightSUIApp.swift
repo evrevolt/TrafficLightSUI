fc49c625f1791aa8a56cdd70708ddd44d078dd72
Gennady Vedernikov <revoltlight@icloud.com>
2022-11-09 16:19:14 +0300

    Initial Commit

---
+++ TrafficLightSUI/TrafficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSUIApp.swift
+//  TrafficLightSUI
+//
+//  Created by Геннадий Ведерников on 09.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
